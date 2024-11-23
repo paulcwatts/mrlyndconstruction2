87a5d5c58ed817dd8fb112643cfec36b632301e1
Paul Watts <paulcwatts@gmail.com>
2024-11-23 13:52:32 -0800

    Add content
    Update to the content from the old site.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Image from "next/image";
 import localFont from "next/font/local";
 
@@ -15,100 +16,112 @@ const geistMono = localFont({
 export default function Home() {
   return (
     <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+      className={`${geistSans.variable} ${geistMono.variable} flex flex-col gap-8`}
     >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+      <Head>
+        <title>
+          MRL Construction - General Contractors: Construction & Remodeling
+          Services
+        </title>
+        <meta
+          name="description"
+          content="MRL Construction, LLC is a Seattle based firm providing home remodeling and additions that meet both aesthetic tastes and budget needs."
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      </Head>
+      <header
+        role="banner"
+        className="h-[400px] text-center flex flex-col bg-blue-700 bg-[url('/img/hillhook1.jpg')] bg-cover bg-center bg-no-repeat w-full"
+      >
+        <div className="my-auto mx-0">
+          <h1 className="text-white text-8xl leading-tight tracking-tight font-bold m-0 [text-shadow:_7px_7px_0_rgb(0_0_0/50%)]">
+            MRL Construction
+          </h1>
+          <h2 className="text-gray-200 mb-0 text-2xl font-bold tracking-tight">
+            General Contractors: Construction & Remodeling Services
+          </h2>
+        </div>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main>
+        <div className="prose prose-xl prose-stone mx-auto">
+          <h2>
+            Beautiful, Custom Home Remodeling <br />
+            and Additions
+          </h2>
+          <p>
+            MRL Construction, LLC is a Seattle based firm providing home
+            remodeling and additions that meet both aesthetic tastes and budget
+            needs. With over 35 years of experience, owner and lead carpenter
+            Mark Lynd is an expert at building the Pacific Northwest home,
+            working with a team of craftspeople that offer complementary
+            carpentry and construction services.
+          </p>
+
+          <p className="text-center">
+            <a
+              className="text-5xl no-underline text-indigo-600"
+              href="tel:+12065470055"
+            >
+              (206) 547-0055
+            </a>
+          </p>
+          <hr />
+          <h2>Additions</h2>
+          <Image
+            src="/img/additions.jpg"
+            alt="Empty home addition"
+            width={1200}
+            height={700}
+          />
+          <p>
+            Additions are part of our special practice. What would you add to
+            your home?
+          </p>
+          <hr />
+          <h2>Remodels</h2>
+          <Image
+            src="/img/remodel.jpg"
+            alt="Remodeled kitchen"
+            width={1024}
+            height={768}
+          />
+          <p>
+            What would you remodel? Kitchen, bathrooms, bedrooms, sunrooms,
+            mudrooms. We work with you to make your house feel like your home.
+          </p>
+          <hr />
+          <h2>Kitchens</h2>
+          <Image
+            src="/img/kitchen.jpg"
+            alt="New kitchen"
+            width={1024}
+            height={768}
+          />
+          <p>
+            We specialize in remodeling kitchens to meet your aesthetic taste
+            and functional needs.
+          </p>
+          <hr />
+          <p>
+            MRL Construction renews and refurbishes your home so that feels
+            right for you. If you’re looking to give your bathroom a fresh look,
+            build an home addition, or redo the whole house, contact MRL
+            Construction for an initial conversation and estimate.
+          </p>
+          <p className="text-center">
+            <a
+              className="text-5xl no-underline text-indigo-600"
+              href="tel:+12065470055"
+            >
+              (206) 547-0055
+            </a>
+          </p>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="mt-24 mb-8 mx-0 text-gray-500 text-center">
+        &copy; {new Date().getFullYear()} MRL Construction, LLC. All Rights
+        Reserved.
       </footer>
     </div>
   );
